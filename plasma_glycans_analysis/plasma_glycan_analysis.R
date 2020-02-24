803aa031497aa236ab93fae40bf40a90fa7762f9
rmakaren <makarenko100@gmail.com>
2020-02-24 12:18:59 +0100

    Add files via upload

--- plasma_glycans_analysis/plasma_glycan_analysis.R
+++ plasma_glycans_analysis/plasma_glycan_analysis.R
@@ -1,3 +1,9 @@
+# GP - glycoprotein
+# This script performs some EDA on the GP data from blood plasma. The dataset consits of
+# mesurements from 31 plates, with additional variables as ethnics, age and gender, batch
+# In order to exclude observation bias from such variables considering their distribution
+# among the plates, the data was randomised and blocked
+
 #set the working directory
 setwd(dir = "/home/rostyslav/Desktop/RSSSO2015/R project/data")
 
@@ -14,26 +20,28 @@ df <- read.csv("plasma.csv")
 #verify the type of the data
 lapply(df, class)
 
-#Ethnic 
+# 1. Ethnic 
 sum(is.na(df$"ethnic")) # number of NA in ethnic column is 10 times higher that actual data
 df$ethnic <- NULL
 
-# gender
+# 2. gender
 table(df$"gender") # the females:males ratio is biased 10 times towards females
 tapply(df$gender, df$Plate, table)
 
-#plate
+# 3. plate
 df_Plate <- table(df$Plate)
 length(table(df$Plate)) #31 Plates
 
-#age
+#4. age
 hist(df$age)
 
 #####################
 # Data visualisation#
 #####################
 
-#Creating boxplot graphics for each glycans gender vs GP
+#5. Variance distribution for each GP vs plate
+############################################
+
 pdf(file="GP_vs_Plates.pdf")
 glycans <- grep("GP", names(df), value=TRUE)
 
@@ -43,7 +51,9 @@ for(glycan in glycans){
 }
 dev.off()
 
-#Creating boxplot graphics for each glycans gender vs GP
+#6. Variance distribution for each GP vs gender
+############################################
+
 pdf(file="GP_vs_age.pdf")
 glycans <- grep("GP", names(df), value=TRUE)
 
@@ -53,43 +63,22 @@ for(glycan in glycans){
 }
 dev.off()
 
-# Excluding the males from the analysis due to number bias
-sub_df <- subset(df, gender == "F")
+#7. Variance distribution for age within the plates
+################################################
 
 pdf(file="age_vs_Plates.pdf")
 boxplot(df$age ~ df$Plate)
 dev.off()
 
+# Excluding the males from the analysis due to observation bias
+sub_df <- subset(df, gender == "F")
+
 pdf(file="age_vs_Plates(no_males).pdf")
 boxplot(sub_df$age ~ sub_df$Plate)
 dev.off()
 
-#Blocking and randomisation by gender and age
-df_f <- df[df$gender == "F", ] # selected by Females
-df_f <- df_f[order(df_f$age),] # sorted by age in Females dataframe
-a = rep(seq(1:31), 42)[1:1278] # created sequence of 31 plates for 42 samples
-nrow(df_f)
-length(a)
-df_f$new_plate = a # assign new plates
-boxplot(df_f$age~df_f$new_plate)
-
-df_m <- df[df$gender == "M", ] # selected by Males
-df_m <- df_m[order(df_m$age),] # sorted by age in Males dataframe
-a = rep(seq(1:31), 4)[1:114] # created sequence of 31 plates for 4 samples
-nrow(df_m)
-length(a)
-df_m$new_plate = a # assign new plates
-boxplot(df_m$age~df_m$new_plate)
-
-df_new <- rbind(df_f, df_m)
-boxplot(df_new$age~df_new$new_plate)
-
-# Making experiment design again
-# replication
-
-df <- read.csv("plasma.csv")
-
-0.1 * 1392 # 10% from orign dataframe is 140
+#8. Blocking and randomisation by gender and age
+################################################
 
 L<-list()
 nrep <- 1000
@@ -100,7 +89,8 @@ for(ind in 1:nrep){
   dupl$gid <- paste0(dupl$gid, "_D")
   df_new <- rbind(df, dupl) # merge picked samples with orign dataframe
   
-  nplates <- 16
+  #nplates <- 16
+  nplates <- 31
   
   #blocking
   df_f <- df_new[df_new$gender == "F", ] # selected by Females from orign
@@ -129,9 +119,6 @@ for(ind in 1:nrep){
 summary(L[1:10])
 x <-as.data.frame(L[1])
 summary(x)
-?as.data.frame()
-
-lapply(L, quantile, probs = 1:3/4, n= TRUE)
 
 x <- rep(0, nrep)
 for (i in 1:length(L)){
